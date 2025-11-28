a0968752cf853ac79024b57ec7faafbf3568967e
Mark Lawrence <marklawrencecatubay@gmail.com>
2025-11-28 22:55:49 +0800

    Fixed bug in uploads middleware

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,38 +1,34 @@
-const multer = require('multer')
-const path = require('path')
+const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        // cb means callback
-        // (err, destination)
-        cb(null, 'public/uploads/')
+        cb(null, 'public/uploads/');
     },
-    file: (req, file, cb) => {
-        // we will create algorithm that makes unique names
+    filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        
-        cb(null, file.fieldName + '-' + uniqueSuffix + path.extname(file.originalname))
+        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
     }
-})
+});
 
 const upload = multer({
-    storage: storage,
-    limits: { fileSize: 10000000 }, // 10 million bytes
-    fileFilter: (req, file, cb) => {
-        checkFileType(file, cb)
-    }
-})
+  storage: storage,
+  limits: { fileSize: 10000000 },
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb);
+  }
+});
 
-const checkFileType = (file, cb) => {
-    const fileTypes = /jpeg|jpg|png|gif/;
-    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase())
-    const mimetype = fileTypes.test(file.mimetype)
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png|gif/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
 
-    if(mimetype && extname) {
-        return cb(null, true)
-    } else {
-        cb('Error: Images only!')
-    }
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images Only!');
+  }
 }
 
-module.exports = upload
+module.exports = upload;
