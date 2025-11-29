0ab71640adae4006039809692c64de2d2599d384
Mark Lawrence <marklawrencecatubay@gmail.com>
2025-11-29 10:12:23 +0800

    Finished the full CRUD API for Products (for the admin).

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -9,8 +9,6 @@ const createProduct = (req, res) => {
 
     const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`
 
-    console.log('Generated Image URL:', imageUrl);
-
     const query = `
         INSERT INTO products (name, description, price, stock_quantity, image_url)
         VALUES (?, ?, ?, ?, ?)
@@ -37,4 +35,94 @@ const createProduct = (req, res) => {
     })
 }
 
-module.exports = { createProduct }
+const getProduct = (req, res) => {
+    const { id } = req.params
+    const query = `
+        SELECT * FROM products
+        WHERE id = ?
+    `
+    const params = [id]
+
+    db.get(query, params, (err, row) => {
+        if(err) {
+            return res.status(500).json({success:false,data:err.message})
+        }
+
+        if(!row) {
+            return res.status(404).json({success:false,data:"Product not found."})
+        } else {
+            return res.status(200).json({success:true,data:row})
+        }
+    })
+}
+
+const getAllProduct = (req, res) => {
+    const query = `
+        SELECT * FROM products
+    `
+    db.all(query, [], (err, rows) => {
+        if(err) {
+            return res.status(500).json({success:false,data:err.message})
+        } else {
+            return res.status(200).json({success:true,data:rows})
+        }
+    })
+}
+
+const updateProduct = (req, res) => {
+    const { id } = req.params
+    const { name, description, price, stock_quantity } = req.body
+
+    let imageUrl = null
+
+    if(req.file) {
+        imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`
+    }
+
+    const query = `
+        UPDATE products 
+        SET
+            name = COALESCE(?, name),
+            description = COALESCE(?, description),
+            price = COALESCE(?, price),
+            stock_quantity = COALESCE(?, stock_quantity),
+            image_url = COALESCE(?, image_url)
+        WHERE id = ?
+    `
+    const params = [name, description, price, stock_quantity, imageUrl, id]
+
+    db.run(query, params, function(err) {
+        if(err) {
+            return res.status(500).json({success:false,data:err.message})
+        }
+
+        if(this.changes > 0) {
+            return res.status(200).json({success:true,data:`Changes to this product no.${id}: ${this.changes}`})
+        } else {
+            return res.status(404).json({success:false,data:"Product not found."})
+        }
+    })
+}
+
+const deleteProduct = (req, res) => {
+    const { id } = req.params
+    const query = `
+        DELETE FROM products
+        WHERE id = ?
+    `
+    const params = [id]
+
+    db.run(query, params, function(err) {
+        if(err) {
+            return res.status(500).json({success:false,data:err.message})
+        }
+
+        if(this.changes > 0) {
+            return res.status(200).json({success:true,data:`Product no.${this.lastID} successfully deleted.`})
+        } else {
+            return res.status(404).json({success:false,data:"Product not found."})
+        }
+    })
+}
+
+module.exports = { createProduct, getProduct, getAllProduct, updateProduct, deleteProduct }
--- src/routes/products.js
+++ src/routes/products.js
@@ -5,5 +5,9 @@ const productController = require('../controllers/productController')
 const upload = require('../middleware/upload')
 
 router.post('/', upload.single('image'), productController.createProduct)
+router.get('/:id', productController.getProduct)
+router.get('/', productController.getAllProduct)
+router.put('/:id', upload.single('image'), productController.updateProduct)
+router.delete('/:id', productController.deleteProduct)
 
 module.exports = router
